025b96d8090f083272203cde594306af14a8e326
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-03-06 17:38:17 +0530

    feat: add security filter for api endpoint

--- Backend/event-management-system/src/main/java/com/technogise/event_management_system/config/SecurityConfig.java
+++ Backend/event-management-system/src/main/java/com/technogise/event_management_system/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.technogise.event_management_system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {        //PasswordEncoder=>This is the return type-> an interface from Spring Security
         return new BCryptPasswordEncoder();            // password hashing function
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable()) 
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/register").permitAll() 
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
     }
 }
