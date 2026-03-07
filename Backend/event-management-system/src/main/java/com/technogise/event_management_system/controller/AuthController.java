73ddfc0f41025b2332868ebf9d6934883e9159e8
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-03-07 11:13:29 +0530

    feat: implement login controller

--- Backend/event-management-system/src/main/java/com/technogise/event_management_system/controller/AuthController.java
+++ Backend/event-management-system/src/main/java/com/technogise/event_management_system/controller/AuthController.java
@@ -3,11 +3,17 @@ package com.technogise.event_management_system.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.technogise.event_management_system.dto.LoginRequest;
 import com.technogise.event_management_system.dto.RegisterRequest;
 import com.technogise.event_management_system.model.User;
+import com.technogise.event_management_system.security.JwtService;
 import com.technogise.event_management_system.service.AuthService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -28,6 +34,27 @@ public class AuthController {
         User user =authService.register(request);
         
         return ResponseEntity.ok(user);
+    }
+
+//LOGIN controller
+    @Autowired
+    private AuthenticationManager authenticationManager; //Spring Security's main interface for authenticating users
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+
+        Authentication authentication =
+                authenticationManager.authenticate(            //This will use our CustomUserDetailsService to load the user by email and compare the provided password with the stored hashed password.
+                        new UsernamePasswordAuthenticationToken( //creates token based on the email and password provided in the login request
+                                request.getEmail(),
+                                request.getPassword()
+                        )
+                );
+
+        return jwtService.generateToken(request.getEmail());
     }
     
 }
