09a62ec863447863718866dd8823c8be640644a2
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-03-07 11:14:52 +0530

    feat: add login request dto

---
+++ Backend/event-management-system/src/main/java/com/technogise/event_management_system/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.technogise.event_management_system.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+
+   
+}
