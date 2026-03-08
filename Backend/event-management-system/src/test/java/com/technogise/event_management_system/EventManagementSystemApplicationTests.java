5993cd9cde9cdafa2205c7c50d14526b6a3d17e6
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-03-08 20:19:52 +0530

    feat: add annotation for in memory db

--- Backend/event-management-system/src/test/java/com/technogise/event_management_system/EventManagementSystemApplicationTests.java
+++ Backend/event-management-system/src/test/java/com/technogise/event_management_system/EventManagementSystemApplicationTests.java
@@ -1,9 +1,11 @@
 package com.technogise.event_management_system;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // This annotation tells Spring Boot not to replace the configured DataSource with an in-memory database during tests.
 class EventManagementSystemApplicationTests {
 
   @Test
